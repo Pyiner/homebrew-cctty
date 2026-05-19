d047bb5ab964fce7cc316f7dcbfeacf2e433c8a3
lijiuyang.5137 <lijiuyang.5137@bytedance.com>
2026-05-20 02:15:31 +0800

    Update cctty v0.1.0 checksums

--- Formula/cctty.rb
+++ Formula/cctty.rb
@@ -7,17 +7,17 @@ class Cctty < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/Pyiner/cctty/releases/download/v0.1.0/cctty-0.1.0-aarch64-apple-darwin.tar.gz"
-      sha256 "9902b775c08a0640c28585488318555ccc588a5585752a2283775ab686046fad"
+      sha256 "c0ac60e7becc61a54b297c40f5028c1d8d9d40fa585955ced70e74838614dfcd"
     else
       url "https://github.com/Pyiner/cctty/releases/download/v0.1.0/cctty-0.1.0-x86_64-apple-darwin.tar.gz"
-      sha256 "0cfe90a93b56ad9c912dd4d9e9b412a7e6526b4f5dab7cd6a5e00be39817a2c2"
+      sha256 "995f6e15b4d92c5a80398a6e8cef0d36b0266abef902def47749f951ff5c78ef"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
       url "https://github.com/Pyiner/cctty/releases/download/v0.1.0/cctty-0.1.0-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "3879e8b74d8016b72a58af93a014537a7194eac93d8f463d260037f0fb3ddca0"
+      sha256 "221e92a960abd54a588ae6e364686c71da68d0eb8c7a8307d5d5ef397bf4e25a"
     end
   end
 
