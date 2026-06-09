76d1040c1dcce66fabd81d8f59595658d445376e
Pyiner <pyiner.io@gmail.com>
2026-06-09 18:10:09 +0800

    Update cctty formula to 0.2.4

--- Formula/cctty.rb
+++ Formula/cctty.rb
@@ -1,23 +1,23 @@
 class Cctty < Formula
   desc "Claude Agent SDK compatibility through the interactive Claude Code TTY"
   homepage "https://github.com/Pyiner/cctty"
-  version "0.2.3"
+  version "0.2.4"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/Pyiner/cctty/releases/download/v0.2.3/cctty-0.2.3-aarch64-apple-darwin.tar.gz"
-      sha256 "4ce971a8d8a04f3117cf5697ab6ad49e015af61ceab2767e6441cfe01dee6407"
+      url "https://github.com/Pyiner/cctty/releases/download/v0.2.4/cctty-0.2.4-aarch64-apple-darwin.tar.gz"
+      sha256 "0bd793ce406ff5c29403cf66b9b41360d01cbf6f8d838db9ab9047c7f7e05798"
     else
-      url "https://github.com/Pyiner/cctty/releases/download/v0.2.3/cctty-0.2.3-x86_64-apple-darwin.tar.gz"
-      sha256 "74fce9841877fab4a4588bf963d634ea23acdb4fa0a2bdbd3c753009cb9ea095"
+      url "https://github.com/Pyiner/cctty/releases/download/v0.2.4/cctty-0.2.4-x86_64-apple-darwin.tar.gz"
+      sha256 "3eff6ab70951f4d9704959db51231e824c3f107a5d0bf34c6399956ad6207ac9"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/Pyiner/cctty/releases/download/v0.2.3/cctty-0.2.3-x86_64-unknown-linux-musl.tar.gz"
-      sha256 "61679d263a8786b116db1870ab4a2c3afb5d705aef8ac697d5e0c2312c789347"
+      url "https://github.com/Pyiner/cctty/releases/download/v0.2.4/cctty-0.2.4-x86_64-unknown-linux-musl.tar.gz"
+      sha256 "7f731775252127c26348d3930c9a6d11cf4a578daafb2875b42af3a3ec43dc40"
     end
   end
 
